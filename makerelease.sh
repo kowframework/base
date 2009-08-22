2110e293c6cdeeb45118233065e5260677785733
Marcelo Coraça de Freitas <ogro@hank.home.kow.com.br>
2009-08-22 15:54:01 -0300

    adding script for creating release packages..

---
+++ makerelease.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+
+
+VERSION=$1
+
+PKGLIST=pkglist
+
+RELEASE_ROOT=$HOME/releases
+
+if [ ! -f $PKGLIST ]
+then
+	echo File not found :: $PKGLIST
+	exit -1
+fi
+
+
+
+for pkg in `cat $PKGLIST`
+do
+	CURR=$PWD
+	PACKAGE=`basename $pkg`
+	VERSION=`cat "$pkg/version"`
+	PKG_VERSION=$PACKAGE-$VERSION
+
+	# This might be good when we have too many releases:
+	#PKG_ROOT=$RELEASE_ROOT/$PACKAGE
+	PKG_ROOT=$RELEASE_ROOT
+	PKG_DIR=$PKG_ROOT/$PKG_VERSION
+	
+	echo -n Working on $PKG_VERSION ...
+
+
+	mkdir -p "$PKG_ROOT"
+	cp -r "$pkg" "$PKG_DIR"
+
+	cd "$PKG_ROOT"
+	rm -rf "$PKG_VERSION/.git"
+	tar zcf "$PKG_VERSION.tar.gz" "$PKG_VERSION"
+	rm -rf "$PKG_VERSION"
+
+
+	cd "$CURR"
+
+	echo DONE
+done
